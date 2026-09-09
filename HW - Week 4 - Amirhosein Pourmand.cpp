0b2712a63e412ee2849f85dbefb657864c05ae64
Amirhosein Pourmand <127144080+amirhoseinpourmand@users.noreply.github.com>
2026-09-09 13:45:24 +0330

    Update HW - Week 4 - Amirhosein Pourmand.cpp

--- HW - Week 4 - Amirhosein Pourmand.cpp
+++ HW - Week 4 - Amirhosein Pourmand.cpp
@@ -30,7 +30,7 @@ main() {
 
 	cout << result;
 }
-// By Amirhosein Pourmand
+
 //==================================================================
 
 //Practice 2:
@@ -53,5 +53,5 @@ main() {
 	
 	cout << "The result is : "<< result;
 }
-// By Amirhosein Pourmand
+
 //==================================================================
